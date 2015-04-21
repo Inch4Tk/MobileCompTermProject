2cab87d490c6dc7822c15cd84e8409db21357c0e
Tobias Gottwald <tobias.gottwald@tum.de>
2015-04-21 22:15:02 +0900

    Add autogenerated webstack

---
+++ Webstack/client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('atTableApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
