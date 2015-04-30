e13f4d840ddb5ce9afc72913007d44720ed77556
Tobias Gottwald <tobias.gottwald@tum.de>
2015-04-30 23:03:13 +0900

    Add android template project with qr code scanner included

---
+++ Android/AtTable/app/src/main/java/kr/ac/kaist/mobile/attable/MainActivity.java
@@ -0,0 +1,63 @@
+package kr.ac.kaist.mobile.attable;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    Button scanButton;
+    TextView scanView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scanButton = (Button) findViewById(R.id.scanbtn);
+        scanView = (TextView) findViewById(R.id.scanview);
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            scanView.setText(scanResult.getContents());
+        }
+    }
+}
